d90ae8b74c291071b29d626da45b7e078a34939d
Philipp Muens <raute1337@gmx.de>
2014-03-10 13:47:02 +0100

    Add up- and downvote functionality
    Add up- and downvote buttons in the languages_table.html-File. Add EventHandler in the languages_table.js-File
    which will call a Meteor.Method. The Meteor-Method gets called on the client- and the server.
    (see "Latency compensation" for more details).

--- collections/languages.js
+++ collections/languages.js
@@ -1,1 +1,10 @@
 Languages = new Meteor.Collection('languages');
+
+Meteor.methods({
+  upvote: function (languageId) {
+    Languages.update(languageId, {$inc: {votes: 1}});
+  },
+  downvote: function (languageId) {
+    Languages.update(languageId, {$inc: {votes: -1}});
+  }
+});
